8b49e828734f60776901b5d10126ca40e7d3e46b
RickRo77 <rrickyroger@gmail.com>
2023-11-06 22:45:43 +0530

    almost finished reports section

--- src/routes/reports/[reportID]/+page.svelte
+++ src/routes/reports/[reportID]/+page.svelte
@@ -2,6 +2,7 @@
     import Dashboard from "../../../components/Dashboard.svelte";
     import { getFirestore, doc, getDoc } from "firebase/firestore";
     import { page } from "$app/stores";
+    import { SAMLAuthProvider } from "firebase/auth";
     const db = getFirestore();
     let name,
         age,
@@ -35,13 +36,16 @@
 
 <Dashboard>
     <div class="InsideDashboard">
-        <table class="report_details">
-            <tr><th>Patient Name: </th> <td>{name}</td></tr>
-            <tr><th>Patient age: </th> <td>{age}</td></tr>
-            <tr><th>Patient gender: </th> <td>{gender}</td></tr>
-            <tr><th>Past Illnesses: </th> <td>{past_disease}</td></tr>
-            <tr><th>Known allergies: </th> <td>{allergies}</td></tr>
+        <h1>Medical Report:-</h1>
+        <h2>Patient details:-</h2>
+        <table class="Report_details">
+            <tr><th>Patient Name: </th> <td>{name}</td><td><img src="edit.png"></td></tr>
+            <tr><th>Patient age: </th> <td>{age}</td><td><img src="edit.png"></td></tr>
+            <tr><th>Patient gender: </th> <td>{gender}</td><td><img src="edit.png"></td></tr>
+            <tr><th>Past Illnesses: </th> <td>{past_disease}</td><td><img src="edit.png"></td></tr>
+            <tr><th>Known allergies: </th> <td>{allergies}</td><td><img src="edit.png"></td></tr>
         </table>
+        <h2>Conversation details:-</h2>
         <table>
             <tr><th>Visible Symptoms: </th> <td>{symptoms}</td></tr>
             <tr><th>Summary: </th> <td>{summary}</td></tr>
@@ -49,4 +53,67 @@
             <tr><th>Predicted Illness: </th> <td>{predicted_illness}</td></tr>
         </table>
     </div>
+
 </Dashboard>
+
+    <style>
+        table, th, td
+        {
+            border-bottom: 0.5px solid transparent;
+            width: 90%;
+            border-color: grey;
+            /* padding: 15px; */
+            
+        }
+        table th,td
+        {
+            padding: 15px;
+            text-align: left;
+        }
+        table th
+        {
+            width:30%;
+        }
+        table
+        {
+            margin: auto;
+            /* margin-top: 10%;
+            margin-bottom: 10%; */
+            border-radius: 5px;
+            box-shadow: 5px 5px 5px rgb(244, 222, 255)
+        }
+        table tr:hover
+        {
+            transition: 0.2s;
+        }
+        .imgimg
+        {width:10%}
+        table tr:hover
+        {
+           transform: scale(1.02);
+           background-color: white;
+        }
+
+        .InsideDashboard h2,h1
+        {
+            padding-top: 4%;
+            padding-left: 4%;
+            padding-right: 4%;
+            padding-bottom: 4%;
+            
+        }
+        .InsideDashboard h1
+        {
+            text-align: center;
+        }
+        .InsideDashboard
+        {
+            background-image: linear-gradient(rgba(141, 141, 141, 0.05rgba(121, 121, 121, 0.05)05));
+        }
+        
+        /* @media screen and (max-width:500px)
+        {
+            dashboard{width: 0%;}
+        } */
+    </style>
+
